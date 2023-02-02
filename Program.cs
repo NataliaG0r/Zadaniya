2029e1d5ef51d311298820d0642f74ff7673210b
Natalia_Go <gorshana@gmail.com>
2023-02-02 21:49:49 +0300

    2 task

--- Program.cs
+++ Program.cs
@@ -8,25 +8,19 @@
 // // 918 -> 1
 
 
-System.Console.WriteLine("Введите число");
-int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
 
 
-if ((Math.Abs(a)) > 100)
-{
-    int b;
-b = (a / 10) % 10;
-System.Console.WriteLine(b);
-}
-else {
-    System.Console.WriteLine(" число не трёхзначное");
-}
-
-// else System.Console.WriteLine($"Число {a} не является трёхзначным");
-
-
-// else 
-
+// if ((Math.Abs(a)) > 100)
+// {
+//     int b;
+// b = (a / 10) % 10;
+// System.Console.WriteLine(b);
+// }
+// else {
+//     System.Console.WriteLine(" число не трёхзначное");
+// }
 
 
 
@@ -38,6 +32,28 @@ else {
 // // 78 -> третьей цифры нет
 
 // // 32679 -> 6
+
+System.Console.WriteLine("Введите число");
+
+string chislo = Console.ReadLine();
+
+int length = chislo.Length;
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (length > 3)
+{
+    int stepen = length-3;
+
+    double third = (number/Math.Pow(10, stepen))%10;
+
+}
+else if (length ==3) double third = number%10;
+
+else System.Console.WriteLine($"В числе {chislo} третьей цифры нет");
+
+
+
 
 // // Задача 15: Напишите программу,
 //  которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
