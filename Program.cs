fbd58283f65dfd9c61b7bed798f15908f01b7df6
Natalia_Go <gorshana@gmail.com>
2023-02-06 12:40:30 +0300

    Task3

--- Program.cs
+++ Program.cs
@@ -37,20 +37,20 @@
 
 
 
-System.Console.WriteLine("Введите число");
- int numb = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число");
+//  int numb = Convert.ToInt32(Console.ReadLine());
  
-if (numb<100) 
-System.Console.WriteLine("В числе нет третьего знака"); 
+// if (numb<100) 
+// System.Console.WriteLine("В числе нет третьего знака"); 
 
-else
-{
-    while (numb>999)
-{
-numb = numb/10;
- }
-int third=numb%10;
- System.Console.WriteLine(third);};
+// else
+// {
+//     while (numb>999)
+// {
+// numb = numb/10;
+//  }
+// int third=numb%10;
+//  System.Console.WriteLine(third);};
 
 
 // // Задача 15: Напишите программу,
@@ -60,3 +60,16 @@ int third=numb%10;
 // 7 -> да
 // 1 -> нет
 
+System.Console.WriteLine("Введите цифру, обозначающую день недели");
+ int day = Convert.ToInt32(Console.ReadLine());
+ if (day>=1&&day<=7)
+ {  if(day==1) System.Console.WriteLine("понедельник");
+ if(day==2) System.Console.WriteLine("вторник");
+ if(day==3) System.Console.WriteLine("среда");
+if(day==4) System.Console.WriteLine("четверг");
+if(day==5) System.Console.WriteLine("пятница");
+if(day==6) System.Console.WriteLine("суббота");
+if(day==7) System.Console.WriteLine("воскресенье");
+ }
+ else
+ System.Console.WriteLine("Такого дня недели не существует");
